6f17d3a5daa237b35ef692ad8305b411764ecf1b
ibrahimilkhan <103080618+ibrahimilkhan@users.noreply.github.com>
2023-04-20 18:49:35 +0300

    Multiton son. Design patterns bitti.

---
+++ Multiton/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton'ın farklı versiyonu Multiton
+
+            // Singleton --> bir nesneden sadece bir tane üretileceği garanti edilir.
+            // Multiton --> single nesneler sözlüğü
+
+            Camera camera1 = Camera.GetCamera("x marka");
+            Camera camera2 = Camera.GetCamera("x marka");
+            Camera camera3 = Camera.GetCamera("y marka");
+            Camera camera4 = Camera.GetCamera("y marka");
+
+            Console.WriteLine(camera1.Id);
+            Console.WriteLine(camera2.Id);
+            Console.WriteLine(camera3.Id);
+            Console.WriteLine(camera4.Id);
+
+            // camera1 - camera2 ve camera3 - camera4 aynı nesnelerdir. 
+
+            Console.ReadLine();
+        }
+    }
+    class Camera
+    {
+        static Dictionary<string, Camera> _cameras = new Dictionary<string, Camera>();
+        static object _lock = new object();
+        public Guid Id { get; set; }
+
+        private Camera()
+        {
+            Id = Guid.NewGuid();
+        }
+        public static Camera GetCamera(string brand)
+        {
+            lock (_lock)
+            {
+                if (!_cameras.ContainsKey(brand))
+                {
+                    _cameras.Add(brand, new Camera());
+                }
+            }
+            return _cameras[brand];
+        }
+    }
+}
---
+++ NullObject/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(StubLogger.GetLogger());
+            customerManager.Save();
+            Console.ReadKey();
+        }
+    }
+
+    class CustomerManager
+    {
+        private ILogger _logger;
+
+        public CustomerManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+            _logger.Log();
+        }
+    }
+
+    interface ILogger
+    {
+        void Log();
+    }
+
+    class Log4NetLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("logged with log4net");
+        }
+    }
+
+    class NLogLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("logged with nloglogger");
+        }
+    }
+
+    class StubLogger : ILogger
+    {
+        private static StubLogger _stubLogger;
+        private static object _lock = new object();
+
+        private StubLogger()
+        {
+
+        }
+        public static StubLogger GetLogger()
+        {
+            lock (_lock)
+            {
+                if (_stubLogger == null)
+                {
+                    _stubLogger = new StubLogger();
+                }
+                return _stubLogger;
+            }
+        }
+        public void Log()
+        {
+        }
+    }
+
+    class CustomerManagerTests
+    {
+        public void Test()
+        {
+            CustomerManager customerManager = new CustomerManager(StubLogger.GetLogger());
+            customerManager.Save();
+        }
+    }
+}
